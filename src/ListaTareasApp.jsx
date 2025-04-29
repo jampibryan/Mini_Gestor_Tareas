9a5f0212251c0a8fbd3670064b49e5356624b769
JampiBryan <jampibryan@gmail.com>
2025-04-29 01:53:32 -0500

    agregar nueva tarea

--- src/ListaTareasApp.jsx
+++ src/ListaTareasApp.jsx
@@ -12,10 +12,23 @@ const listadoTareasInicial = [
 ];
 
 export const ListaTareasApp = () => {
+  const addTask = () => {
+    if (nuevaTarea.trim() === "") return;
+
+    setlistadoTareas([
+      ...listadoTareas,
+      { nombre: nuevaTarea, completado: false },
+    ]);
+    setNuevaTarea("");
+  };
+
   const [listadoTareas, setlistadoTareas] = useState(listadoTareasInicial);
+  const [nuevaTarea, setNuevaTarea] = useState("");
+
   return (
     <>
       <h1>Listado de las tareas</h1>
+
       <ol>
         {listadoTareas.map((item) => (
           <Tarea
@@ -25,6 +38,15 @@ export const ListaTareasApp = () => {
           ></Tarea>
         ))}
       </ol>
+
+      <input
+        placeholder="Escribe una nueva tarea"
+        value={nuevaTarea}
+        onChange={(e) => setNuevaTarea(e.target.value)}
+      ></input>
+      <button onClick={addTask} disabled={nuevaTarea.trim() === ""}>
+        Agregar
+      </button>
     </>
   );
 };
